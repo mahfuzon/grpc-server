a35efb3ff4c601af9d7e147f5f23685552acb975
Mahfuzon Akhiar <mahfuzon0@gmail.com>
2024-12-26 18:50:09 +0700

    feat(bank service): setup migration

--- cmd/main.go
+++ cmd/main.go
@@ -1,11 +1,23 @@
 package main
 
 import (
+	"database/sql"
+	_ "github.com/jackc/pgx/v5/stdlib"
+	"github.com/mahfuzon/grpc-server/db"
 	"github.com/mahfuzon/grpc-server/internal/adapter/grpc"
 	"github.com/mahfuzon/grpc-server/internal/aplication"
+	"log"
 )
 
 func main() {
+
+	sqlDb, err := sql.Open("pgx", "postgres://postgres:123456@localhost:5432/bank_db?sslmode=disable")
+	if err != nil {
+		log.Fatalf("failed to connect to postgres: %v", err)
+	}
+
+	db.Migrate(sqlDb)
+
 	bankService := &aplication.BankService{}
 
 	grpcAdapter := grpc.NewGrpcAdapter(bankService, 9090)
